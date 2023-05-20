0b03fec8c165920dfde08da675e622893aafb415
deepansh <99.deepanshagrawal@gmail.com>
2023-05-20 22:59:03 +0530

    * Added coingecko api call.

--- src/components/Tabs/TabsComponent.jsx
+++ src/components/Tabs/TabsComponent.jsx
@@ -5,7 +5,7 @@ import Tab from "@mui/material/Tab"
 import React, { useState } from "react"
 import "./Tabs.css"
 
-const TabsComponent = () => {
+const TabsComponent = ({ coins }) => {
   const [value, setValue] = useState("grid")
 
   const handleChange = (event, newValue) => {
@@ -20,8 +20,30 @@ const TabsComponent = () => {
           <Tab label="List View" value={"list"} className={"tabButton"} />
         </TabList>
 
-        <TabPanel value={"grid"}>Grid</TabPanel>
-        <TabPanel value={"list"}>List</TabPanel>
+        <TabPanel value={"grid"}>
+          <div>
+            {coins.map((coin, i) => {
+              return (
+                <div key={`coin${i}`}>
+                  <img src={coin.image} alt="symbol" />
+                <p >
+                  {i + 1}. {coin.name}
+                </p>
+
+                </div>
+              )
+            })}
+          </div>
+        </TabPanel>
+        <TabPanel value={"list"}><div>
+            {coins.map((coin, i) => {
+              return (
+                <p key={`coin${i}`}>
+                  {i + 1}. {coin.symbol}
+                </p>
+              )
+            })}
+          </div></TabPanel>
       </TabContext>
     </div>
   )
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,12 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Header from "../components/Common/Header/Header"
 import Tabs from "../components/Tabs/TabsComponent"
+import axios from "axios"
 
 const DashboardPage = () => {
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+    // axios
+    //   .get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
+    //   .then((response) => {
+    //     // console.log(response.data)
+    //     setCoins(response.data)
+    //   })
+    //   .catch((error) => {
+    //     console.log("API Error:", error)
+    //   })
+  }, [])
+
   return (
     <div>
       <Header />
-      <Tabs />
+      <Tabs coins={coins} />
     </div>
   )
 }
