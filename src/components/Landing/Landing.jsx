65dc54c9665ec2b6a22002fb9aa6248179e5965f
deepansh <99.deepanshagrawal@gmail.com>
2023-05-20 17:55:10 +0530

    * Added framer motion.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,23 +2,30 @@ import iphone from "../../assets/iphone.png"
 import gradient from "../../assets/gradient.png"
 import React from "react"
 import Button from "../Common/Button/Button"
+import { motion } from "framer-motion"
 import "./Landing.css"
 
 const Landing = () => {
   return (
     <div className={"flexInfo"}>
       <div className="leftComponent">
-        <h1 className="trackCryptoHeading">Track Crypto</h1>
-        <h1 className="realTimeHeading">Real Time</h1>
-        <p className="infoText">Track crypto through a public api in real time. Visit the dashboard to do so!</p>
-        <div className="btnFlex">
+        <motion.h1 className="trackCryptoHeading" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+          Track Crypto
+        </motion.h1>
+        <motion.h1 className="realTimeHeading" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+          Real Time
+        </motion.h1>
+        <motion.p className="infoText" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 1 }}>
+          Track crypto through a public api in real time. Visit the dashboard to do so!
+        </motion.p>
+        <motion.div className="btnFlex" initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 1.5 }}>
           <Button text={"Dashboard"} />
           <Button text={"Share"} outlined={true} />
-        </div>
+        </motion.div>
       </div>
       <div className="phoneContainer">
-        <img src={iphone} alt="iphone" className="iphoneimg" />
         <img src={gradient} alt="gradient" className="gradientimg" />
+        <motion.img src={iphone} alt="iphone" className="iphoneimg" initial={{ y: -10 }} animate={{ y: 10 }} transition={{ type: "smooth", repeatType: "mirror", duration: 1.5, repeat: Infinity }} />
       </div>
     </div>
   )
