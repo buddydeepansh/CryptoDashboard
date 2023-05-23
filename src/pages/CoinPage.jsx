02399fcd1cf420049c38cd8e2dd0a388fdfdd92a
deepansh <99.deepanshagrawal@gmail.com>
2023-05-23 22:15:58 +0530

    * Initializing coin page.

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react"
+import "./CoinPage.css"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import Loader from "../components/Loader/Loader"
+import Header from "../components/Common/Header/Header"
+import { convertObject } from "../functions/ConvertObject"
+import ListComponent from "../components/ListComponent/ListComponent"
 
 const CoinPage = () => {
+  const { coinId } = useParams()
+  const [coin, setCoin] = useState()
+  const [isLoaded, setisLoaded] = useState(false)
+  useEffect(() => {
+    if (coinId) {
+      axios
+        .get(`https://api.coingecko.com/api/v3/coins/${coinId.toLowerCase()}`)
+        .then((response) => {
+          // console.log(response.data)
+          setCoin(response.data)
+          convertObject(setCoin, response.data)
+          setisLoaded(true)
+        })
+        .catch((error) => {
+          console.log("API Error:", error)
+          setisLoaded(false)
+        })
+    }
+  }, [])
   return (
-    <div>CoinPage</div>
+    <div>
+      <Header />
+      {isLoaded ? (
+        <div className="greyWrapper">
+          <ListComponent coin={coin} />
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </div>
   )
 }
 
