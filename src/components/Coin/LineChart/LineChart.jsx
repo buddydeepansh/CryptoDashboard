865a2bdef0f542bb6576c813ba94df84a03e27ee
deepansh <99.deepanshagrawal@gmail.com>
2023-05-29 22:06:01 +0530

    * Adding y scales.

--- src/components/Coin/LineChart/LineChart.jsx
+++ src/components/Coin/LineChart/LineChart.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Line } from "react-chartjs-2"
 import { Chart as ChartJS } from "chart.js/auto"
+import convertNumber from "../../../functions/ConvertNumber"
 import "./LineChart.css"
 
 const LineChart = ({ chartData, priceType, multiAxis }) => {
@@ -14,6 +15,40 @@ const LineChart = ({ chartData, priceType, multiAxis }) => {
     interaction: {
       mode: "index",
       intersect: false,
+    },
+    scales: {
+      y: {
+        type: "linear",
+        display: true,
+        position: "left",
+        ticks: {
+          callback: function (value) {
+            if (priceType == "total_volumes") {
+              return convertNumber(value);
+            } else if (priceType == "market_caps") {
+              return "$" + convertNumber(value);
+            } else {
+              return "$" + value.toLocaleString();
+            }
+          },
+        },
+      },
+      y2: multiAxis && {
+        type: "linear",
+        display: true,
+        position: "right",
+        ticks: {
+          callback: function (value) {
+            if (priceType == "total_volumes") {
+              return convertNumber(value);
+            } else if (priceType == "market_caps") {
+              return "$" + convertNumber(value);
+            } else {
+              return "$" + value.toLocaleString();
+            }
+          },
+        },
+      },
     },
   }
   return (
